149417add5655f40beff9a9e55f223138b7b7dcd
Paula Mayumi <mayumi_siano10@hotmail.com>
2022-10-27 12:17:42 -0300

    Desenvolvendo a primeira parte do frontweb com React

---
+++ Frontend/blog-pessoal/src/Pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css'
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://tm.ibxk.com.br/2022/02/22/22100833831088.jpg?ims=704x264" alt="Imagem tela home" className="img" />
+        </>        
+    )
+}
+
+export default Home
