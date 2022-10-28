ebd81a0774319d51f32122be5c690affea083017
Paula Mayumi <mayumi_siano10@hotmail.com>
2022-10-28 14:38:08 -0300

    Desenvolvendo páginas de Footer, Navbar e Home

--- Frontend/blog-pessoal/src/App.tsx
+++ Frontend/blog-pessoal/src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import Navbar from './Components/Estaticos/Navbar/Navbar'
+import Footer from './Components/Estaticos/Footer/Footer'
+import {Grid} from '@material-ui/core';
 import Home from './Pages/Home/Home';
 import logo from './logo.svg';
 import './App.css';
 
-
 function App() {
-  return (    
-    <Home />             
+  return (      
+    <>
+      <Navbar />
+        <Home />
+        <Footer />       
+      
+    </>          
   );
 }
 
